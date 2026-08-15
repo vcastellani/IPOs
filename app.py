ab00c3111b78c48b4ac2bd5f39891d85bea3d719
Vincent Castellani <85140598+vcastellani@users.noreply.github.com>
2026-08-15 12:25:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1266,6 +1266,7 @@ with st.sidebar:
     st.markdown(
         "<p style='color:#B0ABA1;font-size:11px;margin-top:24px;'>"
         "IPO data updated August 2026</p>",
+        "Liquidation data updated August 2026</p>",
         unsafe_allow_html=True,
     )
 
